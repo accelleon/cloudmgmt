f2f91652f8c1e0c0f64a7ea70f99949c88c60a44
Anthony Wilkins <accelleon@live.com>
2022-03-17 13:11:59 -0400

    Started login page work

---
+++ frontend/src/layouts/LoginLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <q-layout view="lHh Lpr lFf" class="img-background">
+        <q-page-container class="container-fluid">
+            <router-view />
+        </q-page-container>
+    </q-layout>
+</template>
+
+<style scoped>
+::-webkit-scrollbar {
+    display: none;
+}
+.img-background {
+    background: #fff;
+    height: 100%;
+}
+</style>
