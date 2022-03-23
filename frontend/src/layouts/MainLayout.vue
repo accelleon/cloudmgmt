9c8f5660c329d9186a83e6e6b15fa32dde2ffa4f
Anthony Wilkins <accelleon@live.com>
2022-03-23 21:04:00 +0000

    Mocking of main layout

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -2,31 +2,8 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-btn
-          flat
-          dense
-          label="Enable Twofa"
-          @click="enableTwoFa"
-        />
-
-        <q-btn
-          flat
-          dense
-          label="Disable Twofa"
-          @click="disableTwoFa"
-        />
-
         <q-toolbar-title>
-          Quasar App
+          Cloud Management
         </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
@@ -38,19 +15,127 @@
       show-if-above
       bordered
     >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+      <q-card square class="absolute-top full-width row" style="height: 150px">
+        <div class="col-10 q-pa-lg text-h6">{{ user.fullname }}</div>
+        <div class="col-2" align="right">
+          <div class="q-pa-sm">
+            <q-btn
+              rounded
+              padding="none"
+              @click="pinned = !pinned"
+            >
+              <q-icon
+                :name="pinned ? 'push_pin' : 'o_push_pin'"
+                :class="pinned ? '' : 'rotate-90'"
+              />
+              <q-tooltip anchor="center right" self="center left" :offset="[10, 10]">
+                Pin/Unpin the navigation drawer.
+              </q-tooltip>
+            </q-btn>
+          </div>
+        </div>
+        <q-separator/>
+        <q-card-actions>
+          <q-btn
+            square
+            label="Profile"
+            icon="manage_accounts"
+          />
+        </q-card-actions>
+      </q-card>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+      <q-scroll-area style="height: calc(100% - 220px); margin-top: 150px; margin-bottom: 70px; border-right: 1px solid #ddd">
+        <q-list padding>
+          <q-item
+            clickable
+            v-ripple
+            :active="link === 'billing'"
+            @click="link = 'billing'"
+            active-class="my-menu-link"
+          >
+            <q-item-section avatar>
+              <q-icon name="receipt_long" />
+            </q-item-section>
+            <q-item-section>Billing</q-item-section>
+          </q-item>
+
+          <q-item
+            clickable
+            v-ripple
+            :active="link === 'purge'"
+            @click="link = 'purge'"
+            active-class="my-menu-link"
+          >
+            <q-item-section avatar>
+              <q-icon name="delete_sweep" />
+            </q-item-section>
+            <q-item-section>Purge</q-item-section>
+          </q-item>
+
+          <q-item
+            clickable
+            v-ripple
+            :active="link === 'users'"
+            @click="link = 'users'"
+            active-class="my-menu-link"
+          >
+            <q-item-section avatar>
+              <q-icon name="manage_accounts" />
+            </q-item-section>
+            <q-item-section>Users</q-item-section>
+          </q-item>
+
+          <q-item
+            clickable
+            v-ripple
+            :active="link === 'groups'"
+            @click="link = 'groups'"
+            active-class="my-menu-link"
+          >
+            <q-item-section avatar>
+              <q-icon name="groups" />
+            </q-item-section>
+            <q-item-section>Groups</q-item-section>
+          </q-item>
+
+          <q-item
+            clickable
+            v-ripple
+            :active="link === 'accounts'"
+            @click="link = 'accounts'"
+            active-class="my-menu-link"
+          >
+            <q-item-section avatar>
+              <q-icon name="cloud" />
+            </q-item-section>
+            <q-item-section>Accounts</q-item-section>
+          </q-item>
+
+          <q-item
+            clickable
+            v-ripple
+            :active="link === 'settings'"
+            @click="link = 'settings'"
+            active-class="my-menu-link"
+          >
+            <q-item-section avatar>
+              <q-icon name="settings" />
+            </q-item-section>
+            <q-item-section>Settings</q-item-section>
+          </q-item>
+
+        </q-list>
+      </q-scroll-area>
+
+      <div class="absolute-bottom justify-center row" style="height: 70px">
+        <div class="column q-pa-lg">
+          <q-btn
+            square
+            label="Log out"
+            icon="logout"
+          />
+        </div>
+      </div>
     </q-drawer>
 
     <q-page-container>
@@ -59,74 +144,35 @@
   </q-layout>
 </template>
 
+<style lang="scss">
+.my-menu-link {
+  color: black;
+  background: #fde08e;
+}
+</style>
+
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import EssentialLink from 'components/EssentialLink.vue';
 import { useQuasar } from 'quasar';
 import TwoFaDialog from 'src/components/TwoFaDialog.vue';
 import { useUserStore } from 'src/stores/user-store';
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
 export default defineComponent({
   name: 'MainLayout',
 
-  components: {
-    EssentialLink
-  },
-
   setup () {
-    const leftDrawerOpen = ref(false)
+    const leftDrawerOpen = ref(false);
+    const pinned = ref(true);
+    const link = ref('');
     const $q = useQuasar();
     const user = useUserStore();
 
     user.getUser();
 
     return {
-      essentialLinks: linksList,
+      pinned,
+      link,
+      user,
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
