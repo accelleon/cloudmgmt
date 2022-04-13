e2bd5ad19a152f065ad8eebe885f571f05a97985
Anthony Wilkins <accelleon@live.com>
2022-04-13 18:27:45 +0000

    Accounts UI

---
+++ frontend/src/components/dialogs/UpdateAccountDialog.vue
@@ -0,0 +1,181 @@
+<template>
+  <q-dialog ref="dialogRef" @hide="onHide">
+    <div class="row window-width">
+      <q-card class="q-dialog-plugin">
+        <q-card-section>
+          <div class="text-h5">Edit Account</div>
+        </q-card-section>
+
+        <q-separator />
+
+        <q-card-section>
+          <q-tabs v-model="tab">
+            <q-tab name="1" label="General" />
+            <q-tab name="2" label="Security" />
+          </q-tabs>
+
+          <q-tab-panels
+            class="q-py-md"
+            v-model="tab"
+            animated
+            swipeable
+            keep-alive
+          >
+            <q-tab-panel name="1">
+              <q-form ref="gen">
+                <q-select
+                  class="full-width"
+                  square
+                  filled
+                  readonly
+                  v-model="iaas"
+                  label="Provider"
+                  option-label="name"
+                  map-options
+                  :options="iaasOptions"
+                  lazy-rules
+                  :rules="[(val: any) => (val) || 'Provider is required']"
+                />
+                <q-input
+                  class="full-width"
+                  ref="accountName"
+                  name="accountName"
+                  square
+                  filled
+                  v-model="account.name"
+                  label="Account Name"
+                  autofocus
+                />
+              </q-form>
+            </q-tab-panel>
+            <q-tab-panel name="2">
+              <q-form ref="sec">
+                <div v-if="iaas" class="q-px-none">
+                  <div v-for="param in iaas.params" :key="param.key">
+                    <q-input
+                      v-if="param.type === 'string' || param.type === 'secret'"
+                      v-model="account.data[param.key]"
+                      class="full-width q-py-md"
+                      filled
+                      square
+                      :label="param.label"
+                    />
+                    <q-select
+                      v-if="param.type === 'choice'"
+                      v-model="account.data[param.key]"
+                      class="full-width q-py-md"
+                      filled
+                      square
+                      :label="param.label"
+                      :options="param.choices"
+                    />
+                  </div>
+                </div>
+              </q-form>
+            </q-tab-panel>
+          </q-tab-panels>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn color="primary" label="Cancel" @click="onHide" />
+          <q-btn color="primary" label="Submit" @click="onSubmit" />
+        </q-card-actions>
+      </q-card>
+    </div>
+  </q-dialog>
+</template>
+
+<script lang="ts">
+import { useDialogPluginComponent, useQuasar } from 'quasar';
+import { defineComponent, ref, onMounted, computed, PropType } from 'vue';
+import _ from 'lodash';
+import { Account } from 'src/models/Account';
+import { AccountService } from 'src/services/AccountService';
+import { ProviderService } from 'src/services/ProviderService';
+import { UpdateAccount } from 'src/models/UpdateAccount';
+import { Iaas } from 'src/models/Iaas';
+
+export default defineComponent({
+  emits: [...useDialogPluginComponent.emits],
+
+  props: {
+    old: {
+      type: Object as PropType<Account>,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } =
+      useDialogPluginComponent();
+
+    const $q = useQuasar();
+    const account = ref<UpdateAccount>();
+    const iaas = ref();
+    const tab = ref('1');
+    const gen = ref();
+    const sec = ref();
+
+    onMounted(() => {
+      iaas.value = props.old.iaas;
+      account.value = {
+        name: props.old.name,
+        data: { ...props.old.data },
+      };
+    });
+
+    return {
+      dialogRef,
+      account,
+      gen,
+      sec,
+      iaas,
+      tab,
+
+      onHide() {
+        onDialogHide();
+      },
+
+      async onSubmit() {
+        if (
+          _.isEqual(account.value, {
+            name: props.old.name,
+            data: props.old.data,
+          })
+        ) {
+          $q.notify({
+            message: 'No changes made',
+            color: 'positive',
+          });
+          onDialogCancel();
+          return;
+        }
+        if (await gen.value.validate()) {
+          if (sec.value) {
+            if (!(await sec.value.validate())) {
+              return;
+            }
+          }
+          AccountService.updateAccount(props.old.id!, { ...account.value })
+            .then(() => {
+              $q.notify({
+                message: 'Account updated successfully',
+                color: 'positive',
+              });
+              onDialogOK();
+            })
+            .catch((err: any) => {
+              $q.notify({
+                message: err.body?.detail || err.message,
+                color: 'negative',
+              });
+            });
+        }
+      },
+
+      onCancel() {
+        onDialogCancel();
+      },
+    };
+  },
+});
+</script>
---
+++ frontend/src/pages/AccountsPage.vue
@@ -0,0 +1,296 @@
+<template>
+  <q-page>
+    <div class="q-pa-xl">
+      <q-table
+        class="acct-table"
+        row-key="id"
+        binary-state-sort
+        virtual-scroll
+        :virtual-scroll-sticky-size-start="48"
+        v-model:pagination="pagination"
+        :rows="accts"
+        :columns="columns"
+        :loading="loading"
+        @request="onRequest"
+      >
+        <template v-slot:top-left>
+          <div class="row">
+            <q-input
+              style="width: 25vw"
+              v-model="filter.name"
+              clearable
+              dense
+              flat
+              hide-underline
+              placeholder="Search"
+              debounce="500"
+              type="search"
+              @update:model-value="onSearch"
+            >
+              <template v-slot:prepend>
+                <q-icon name="search" />
+              </template>
+            </q-input>
+            <q-btn-dropdown dropdown-icon="filter_list" flat @hide="onSearch">
+              <q-list>
+                <q-item>
+                  <q-item-section avatar>
+                    <q-toggle
+                      v-model="filterBy.iaas"
+                      flat
+                      dense
+                      @update:model-value="onSearch"
+                    />
+                  </q-item-section>
+                  <q-item-section>Provider</q-item-section>
+                  <q-item-section side>
+                    <q-select
+                      style="min-width: 100px"
+                      v-model="filter.iaas"
+                      flat
+                      dense
+                      :options="iaas"
+                      @update:model-value="onSearch()"
+                    />
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-btn-dropdown>
+          </div>
+        </template>
+
+        <template v-slot:top-right>
+          <q-btn icon="add" @click="onAdd">
+            <q-tooltip> Add Account </q-tooltip>
+          </q-btn>
+        </template>
+
+        <template v-slot:body-cell-action="props">
+          <q-td :props="props">
+            <q-btn-dropdown dropdown-icon="more_vert" flat auto-close>
+              <q-list>
+                <q-item clickable @click="onEdit(props.row)">
+                  <q-item-section avatar>
+                    <q-icon name="edit" size="xs" />
+                  </q-item-section>
+                  <q-item-section>Edit</q-item-section>
+                </q-item>
+                <q-item clickable @click="onDelete(props.row)">
+                  <q-item-section avatar>
+                    <q-icon name="delete" size="xs" />
+                  </q-item-section>
+                  <q-item-section>Delete</q-item-section>
+                </q-item>
+              </q-list>
+            </q-btn-dropdown>
+          </q-td>
+        </template>
+      </q-table>
+    </div>
+  </q-page>
+</template>
+
+<style lang="scss">
+.acct-table {
+  height: calc(100vh - 160px);
+
+  .q-table__top,
+  .q-table__bottom,
+  thead tr:first-child th {
+    background-color: #f5f5f5;
+  }
+
+  thead tr th {
+    position: sticky;
+    z-index: 1;
+  }
+
+  thead tr:last-child th {
+    top: 48px;
+  }
+  thead tr:first-child th {
+    top: 0;
+  }
+}
+</style>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted, PropType } from 'vue';
+import { useQuasar } from 'quasar';
+import { AccountService } from '..';
+import { ProviderService } from '..';
+import { Account } from '../models/Account';
+import { Iaas } from '../models/Iaas';
+import { SearchOrder } from '../models/SearchOrder';
+import NewAccountDialog from 'src/components/dialogs/NewAccountDialog.vue';
+import UpdateAccountDialog from 'src/components/dialogs/UpdateAccountDialog.vue';
+
+const columns = [
+  {
+    name: 'name',
+    required: true,
+    label: 'Name',
+    align: 'left',
+    sortable: true,
+    field: 'name',
+  },
+  {
+    name: 'iaas',
+    required: true,
+    label: 'Provider',
+    align: 'left',
+    sortable: true,
+    field: (row: any) => row.iaas.name,
+  },
+  {
+    name: 'action',
+    label: '',
+    align: 'right',
+    sortable: false,
+    field: 'action',
+  },
+];
+
+interface Filter {
+  name?: string;
+  iaas?: string;
+}
+
+export default defineComponent({
+  name: 'AccountsPage',
+
+  setup() {
+    const $q = useQuasar();
+
+    const accts = ref<Account[]>();
+    const iaas = ref<string[]>();
+    const filter = ref({
+      name: '',
+      iaas: '',
+    } as Filter);
+    const filterBy = ref({
+      iaas: false,
+    });
+    const loading = ref(false);
+    const pagination = ref({
+      sortBy: 'name',
+      descending: false,
+      page: 1,
+      rowsPerPage: 20,
+      rowsNumber: 20,
+    });
+
+    const onRequest = (props: any) => {
+      const { page, rowsPerPage, sortBy, descending } = props.pagination;
+
+      loading.value = true;
+
+      ProviderService.getProviders();
+
+      return AccountService.getAccounts(
+        filter.value.name,
+        filterBy.value.iaas ? filter.value.iaas : undefined,
+        page - 1,
+        rowsPerPage,
+        sortBy,
+        descending ? SearchOrder.DESC : SearchOrder.ASC
+      )
+        .then(({ results, total }) => {
+          iaas.value = [...new Set(results.map((a) => a.iaas.name))];
+          pagination.value.rowsNumber = total;
+          pagination.value.page = page;
+          pagination.value.rowsPerPage = rowsPerPage;
+          pagination.value.sortBy = sortBy;
+          pagination.value.descending = descending;
+          accts.value = results;
+        })
+        .catch((err) => {
+          $q.notify({
+            color: 'negative',
+            textColor: 'white',
+            message: err.body?.detail || err.message,
+          });
+        })
+        .finally(() => {
+          loading.value = false;
+        });
+    };
+
+    onMounted(() => {
+      onRequest({ pagination: pagination.value });
+    });
+
+    const onDelete = (props: any) => {
+      const { id, name } = props;
+      $q.dialog({
+        title: 'Delete Account',
+        message: `Are you sure you want to delete ${name}?`,
+        color: 'negative',
+      }).onOk(() => {
+        loading.value = true;
+        AccountService.deleteAccount(id)
+          .then(() => {
+            loading.value = false;
+            $q.notify({
+              color: 'positive',
+              textColor: 'white',
+              message: `${name} deleted`,
+            });
+            onRequest({ pagination: pagination.value });
+          })
+          .catch((err) => {
+            loading.value = false;
+            $q.notify({
+              color: 'negative',
+              textColor: 'white',
+              message: err.body?.detail || err.message,
+            });
+          });
+      });
+    };
+
+    const onEdit = (props: any) => {
+      $q.dialog({
+        title: 'Edit Account',
+        component: UpdateAccountDialog,
+        componentProps: {
+          old: { ...props },
+        },
+      }).onOk(() => {
+        onRequest({
+          pagination: pagination.value,
+        });
+      });
+    };
+
+    const onAdd = () => {
+      $q.dialog({
+        title: 'New Account',
+        component: NewAccountDialog,
+      }).onOk((acct: Account) => {
+        onRequest({ pagination: pagination.value });
+      });
+    };
+
+    const onSearch = (value: any) => {
+      if (value === 'string' || value === 'null') filter.value.name = value;
+      onRequest({ pagination: pagination.value });
+    };
+
+    return {
+      accts,
+      columns,
+      filter,
+      filterBy,
+      loading,
+      iaas,
+      pagination,
+      onRequest,
+      onDelete,
+      onEdit,
+      onAdd,
+      onSearch,
+    };
+  },
+});
+</script>
