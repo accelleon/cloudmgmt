12a4be9fd41b781384c7d28f1b333d8c79f5de51
Anthony Wilkins <accelleon@live.com>
2022-04-29 14:34:11 +0000

    Account data filter update

--- frontend/src/models/AccountData.ts
+++ frontend/src/models/AccountData.ts
@@ -4,6 +4,10 @@
 
 export type AccountData = {
     access_key?: string;
+    subscription_id?: string;
+    tenant_id?: string;
+    client_id?: string;
+    account_name?: string;
     endpoint?: string;
     app_key?: string;
     consumer_key?: string;
