f5b1aecbf62dbb27cf2e7096b66ad6e11c351b41
Anthony Wilkins <accelleon@live.com>
2022-05-05 17:17:01 +0000

    Fix account constraints Whoops....

---
+++ backend/app/alembic/versions/120ab9341a2f_fix_account_constraints.py
@@ -0,0 +1,32 @@
+"""fix account constraints
+
+Revision ID: 120ab9341a2f
+Revises: b48a98428f2b
+Create Date: 2022-05-05 17:15:35.812834
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '120ab9341a2f'
+down_revision = 'b48a98428f2b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_index('iaas_id')
+    op.drop_index('ix_account_name')
+
+    op.create_index(
+        "ix_account_name_iaas",
+        "account",
+        ["name", "iaas_id"],
+        unique=True,
+    )
+
+
+def downgrade():
+    pass
