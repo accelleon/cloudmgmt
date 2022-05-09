fa82be65b6081846d5a51f2dfbe064731ac33728
Anthony Wilkins <accelleon@live.com>
2022-05-09 19:39:33 +0000

    Fix: All async tasks runs in 1 loop per worker

--- backend/app/app/tasks/utils.py
+++ backend/app/app/tasks/utils.py
@@ -1,15 +1,14 @@
 import asyncio
 
+loop = None
+
 
 # Decorator to run async function as sync
 def run_sync(func):
     def wrapper(*args, **kwargs):
-        # TODO: Gotta be a less hacky way to do this
-        # get_event_loop() doesn't work here for multiple functions
-        # because of a race condition where we close the loop after the
-        # next task grabs it, but before it finishes
-        loop = asyncio.new_event_loop()
-        loop.run_until_complete(func(*args, **kwargs))
-        loop.close()
+        global loop
+        if loop is None:
+            loop = asyncio.get_event_loop()
+        return loop.run_until_complete(func(*args, **kwargs))
 
     return wrapper
